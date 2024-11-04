a42dd6745b9e747f281846579030cb28be402351
Nathnael Yirga <Nathnaelyirga@gmail.com>
2024-11-04 20:12:49 +0300

    new bakend:

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 
 const  axiosBase = axios.create({
+    baseURL:"https://evangadi-forum-back-end-1-vwru.onrender.com/api"
     //baseURL:"http://localhost:3003/api"
-    baseURL:"https://evangadi-forum-back-end-zl28.onrender.com/api"
+    //baseURL:"https://evangadi-forum-back-end-zl28.onrender.com/api"
 //https://evangadi-forum-back-end-zl28.onrender.com
 
 })
